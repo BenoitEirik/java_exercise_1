ce975404c1d8e81065e6c1114910c75a18e4a903
BenoitEirik <olafsergent@gmail.com>
2021-11-30 22:27:58 +0100

    Add freq function

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,8 @@
-import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Launcher {
     public static void main (String[] args) {
@@ -25,6 +29,32 @@ public class Launcher {
                 }
                 else {
                     System.out.println("Fib(" + integer + ") = " + fibonacci(integer));
+                }
+            }
+            else if ("freq".equals(nextline)) {
+                System.out.println("Enter a file path:");
+                Path path = null;
+                try {
+                    path = Paths.get(shell.nextLine());
+                    String file = java.nio.file.Files.readString(path);
+
+                    String[] words = file.split(" ");
+                    Map<String, Long> nbOccur = Arrays.stream(words)
+                            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+                    Map<String, Long> finalMap = new LinkedHashMap<>();
+                    nbOccur.entrySet().stream()
+                            .sorted(Map.Entry.<String, Long>comparingByValue()
+                                    .reversed()).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+                    var arrayOccur = finalMap.entrySet().stream().limit(3).toArray();
+
+                    System.out.println("Last three occurences = " + arrayOccur[0].toString().split("=")[0] + ", "
+                    + arrayOccur[1].toString().split("=")[0] + ", "
+                    + arrayOccur[2].toString().split("=")[0]);
+                }
+                catch (Exception exception) {
+                    System.out.println("unreadable file : ");
+                    exception.getClass().getSimpleName();
                 }
             }
             else {
